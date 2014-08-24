8aa22385f80f97a1679b39ff2fefed28843d1331
maggarwal <maggarwal@amplify.com>
2014-08-23 21:22:45 -0400

    fixed the problem of symlink

--- fidget.sh
+++ fidget.sh
@@ -12,7 +12,16 @@ fi
 
 echo "Creating fiddle to $FIDGET"
 (set -e;
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+# get the actual file path avoiding symlinks
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+
+echo $DIR
 mkdir $FIDGET;
 cp $DIR/template/* $FIDGET;
 echo $FIDGET
