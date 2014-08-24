378f1db09f4428efbd70ab98c003e395cba6ab1c
maggarwal <maggarwal@amplify.com>
2014-08-23 21:08:13 -0400

    fixed the wrong dir name in the script

--- fidget.sh
+++ fidget.sh
@@ -10,24 +10,11 @@ else
     FIDGET=$FIDGETDIR/$1
 fi
 
-VIM=mvim
-
-function run_fiddle {
-  $VIM -S fiddle.vim
-}
-
-if [ -e $FIDGET ]; then
-    echo "Fiddle already exists, running it..."
-    (set -e;
-      cd $FIDGET;
-      run_fiddle
-    )
-else
-    echo "Creating fiddle to $FIDGET"
-    (set -e;
-        mkdir $FIDGET;
-        cp template/* $FIDGET;
-        echo $FIDGET
-        node node_server.js $FIDGET >/dev/null 2>/dev/null &
-    )
-fi
+echo "Creating fiddle to $FIDGET"
+(set -e;
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+mkdir $FIDGET;
+cp $DIR/template/* $FIDGET;
+echo $FIDGET
+node node_server.js $FIDGET >/dev/null 2>/dev/null &
+)
